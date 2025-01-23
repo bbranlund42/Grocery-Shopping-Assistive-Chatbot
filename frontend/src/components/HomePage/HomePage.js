c9199b65eaf13d6612f7b999d68004759e41243b
Noah Herron <noahherron2002@gmail.com>
2025-01-23 13:33:24 -0600

    added correct page location name in HomePage.js

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -45,7 +45,7 @@ const HomePage = () => {
             className="w-full text-left text-blue-600 py-2 px-3 rounded-md hover:bg-blue-100 hover:shadow-md transition-all duration-200"
             onClick={() => navigate("/JustChatting")}
           >
-            Option 2
+            Just Chatting
           </button>
           <button
             className="w-full text-left text-blue-600 py-2 px-3 rounded-md hover:bg-blue-100 hover:shadow-md transition-all duration-200"
