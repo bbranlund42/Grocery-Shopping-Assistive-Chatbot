b19bbeda9084719c3465c2a257f88d576fe4a927
Peyton Rushing <pcrushing42@tntech.edu>
2025-03-26 22:41:11 -0500

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -70,26 +70,28 @@ app.post('/addNewFood', async (req,res) => {
         const {product_id, product_name, category, quantity, price, description, location} = req.query;
         const newFood = new Food({product_id, product_name, category, quantity, price, description, location});
 
+        const savedFood = await newFood.save();
+
         const embedding = await embedding_model.embedQuery(newFood['product_name']); 
-        const res = await Food.updateOne(
-          {'_id': newFood['_id']}, 
+        const result = await Food.updateOne(
+          {'product_id': newFood['product_id']}, 
           {$set: {'embedding': embedding} }, 
           {strict: false}
           ); 
           const text = (`Product ID: ${newFood['product_id']}
-            Product Name: ${newFood['product_name']}
-            Category: ${newFood['category']}
-            Quantity: ${newFood['quantity']}
-            Price: ${newFood['price']}
-            Description: ${newFood['description']}`
+Product Name: ${newFood['product_name']}
+Category: ${newFood['category']}
+Quantity: ${newFood['quantity']}
+Price: ${newFood['price']}
+Description: ${newFood['description']}
+Location: $`
         ); 
         const res2 = await Food.updateOne(
-            {'_id': newFood['_id']}, 
+            {'product_id': newFood['product_id']}, 
             {$set: {'text': text}}, 
             {strict: false}
         ); 
 
-        const savedFood = await newFood.save();
         res.status(201).json({message:"Added Successfully",data: savedFood});
     } catch (error) {
         res.status(500).json({ error: error.message });
